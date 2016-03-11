5e073b5b6462eee1cc070f9ac1c27e174f3e1844
Horst <info@lbm-services.de>
2016-03-11 16:54:08 +0100

    mod_thick_rss.php updated

--- mod_thick_rss.php
+++ mod_thick_rss.php
@@ -1,21 +1,24 @@
 <?php
 /**
-* @file mod_thick_rss.php  $Format:%ci$ 
+* @file mod_thick_rss.php  2013-10-25 11:02:23 +0200 
 * @package mod_thick_rss
 * @author Horst Lindlbauer info@lbm-services.de
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
-* @version 3.4 (build $version$)
-* @credit: Boris Popoff (smoothbox), Ryan Parman, Geoffrey Sneddon (SimplePie), Codey Lindley (Thickbox), David Thomas (Slick RSS)
+* @ $version: 3.4$
 * @description: Joomla module to show list of RSS feeds and link to news in modal window
 **/
 // no direct access
+
+DEFINE('DS', DIRECTORY_SEPARATOR);
+
 defined('_JEXEC') or die('Restricted access');
 // Include the helper functions only once
 require_once (dirname(__FILE__).DS.'helper.php');
 
 
 // load Headerscripts
-$instance= modThickRSSHelper::setHeader();
+
+$instance= modThickRSSHelper::setHeader($params);
 
 // Get data from helper class
 $rss_content = modThickRSSHelper::getFeed($params);
